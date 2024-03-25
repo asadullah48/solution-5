858bd3de5335786d2e4b903d011014aea7796802
ASADULLAH SHAFIQUE <asadullahshafique@hotmail.com>
2024-03-25 11:05:25 +0500

    maths
    basic math

---
+++ s5.ts
@@ -0,0 +1,5 @@
+console.log(9 + 11); // Addition
+console.log(13 - 8); // Subtraction
+console.log(39 * 3); // Multiplication
+console.log(108 / 2); // Division
+console.log(108 % 50); // Percentage
