e9e9389b188e20e4cb41c0c43fc9e1810b115acb
ASADULLAH SHAFIQUE <asadullahshafique@hotmail.com>
2024-03-29 01:55:06 +0500

    made changes

--- s5.ts
+++ s5.ts
@@ -1,5 +1,8 @@
-console.log(9 + 11); // Addition
-console.log(13 - 8); // Subtraction
-console.log(39 * 3); // Multiplication
-console.log(108 / 2); // Division
-console.log(108 % 50); // Percentage
+console.log(3 + 5); // Addition
+console.log(16 - 8); // Subtraction
+console.log(4 * 2); // Multiplication
+console.log(16 / 2); // Division
+
+//Number Eight: Write addition, subtraction, multiplication, and division 
+//operations that each result in the number 8. 
+//Be sure to enclose your operations in print statements to see the results.
