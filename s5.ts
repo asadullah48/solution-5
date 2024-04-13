5a67ae39d1824c68b25c95f8e3f1c2c2bc82a81c
ASADULLAH SHAFIQUE <asadullahshafique@hotmail.com>
2024-04-13 10:24:40 +0500

    change done

--- s5.ts
+++ s5.ts
@@ -6,3 +6,11 @@ console.log(16 / 2); // Division
 //Number Eight: Write addition, subtraction, multiplication, and division 
 //operations that each result in the number 8. 
 //Be sure to enclose your operations in print statements to see the results.
+console.log(4 + 4); 
+console.log(100 - 92); 
+console.log(8 * 1); 
+console.log(64 / 8); 
+console.log(6 + 2); 
+console.log(8 - 0); 
+console.log(8 * 1); 
+console.log(96 / 12); 
